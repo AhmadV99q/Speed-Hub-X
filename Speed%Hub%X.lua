f894c99343633a33f8aea0b96c0b31045bebdbe8
AhmadV99q <raizo8raizo@gmail.com>
2025-08-31 23:01:32 +0800

    Update Speed%Hub%X.lua

--- Speed%Hub%X.lua
+++ Speed%Hub%X.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://github.com/AhmadV99q/Ewan-/blob/main/Ewan2 "))()
+loadstring(game:HttpGet("https://pastebin.com/raw/wdA5NLMZ "))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
